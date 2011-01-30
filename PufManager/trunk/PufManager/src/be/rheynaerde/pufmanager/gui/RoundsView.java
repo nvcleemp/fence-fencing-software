40c4ef39ef7ecf93bdbf04adcf7eb45d54fb465c
nico.vancleemput <nico.vancleemput@00d01c20-5a78-ff6a-2b06-9238665b289a>
2011-01-30 09:10:16 +0000

    RoundsView implements Scrollable

--- PufManager/trunk/PufManager/src/be/rheynaerde/pufmanager/gui/RoundsView.java
+++ PufManager/trunk/PufManager/src/be/rheynaerde/pufmanager/gui/RoundsView.java
@@ -27,15 +27,19 @@ import be.rheynaerde.pufmanager.data.Team;
 import be.rheynaerde.pufmanager.data.listener.CompetitionListener;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.Rectangle;
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
+import javax.swing.Scrollable;
+import javax.swing.SwingConstants;
 
 /**
  *
  * @author nvcleemp
  */
-public class RoundsView extends JPanel implements CompetitionListener{
+public class RoundsView extends JPanel implements CompetitionListener, Scrollable{
 
     private Competition competition;
 
@@ -73,5 +77,154 @@ public class RoundsView extends JPanel implements CompetitionListener{
 
     public void roundsChanged() {
         buildView();
+    }
+
+    public Dimension getPreferredScrollableViewportSize() {
+        return getPreferredSize();
+    }
+
+    public int getScrollableUnitIncrement(Rectangle visibleRect, int orientation, int direction) {
+        if(visibleRect!=null
+                && (orientation==SwingConstants.HORIZONTAL
+                    || orientation==SwingConstants.VERTICAL)){
+            int roundPanel = locationToRoundPanel(visibleRect.y);
+            if(roundPanel==-1){
+                return 0;
+            } else {
+                if (direction > 0) { // i.e. south
+                    if(roundPanel==getComponentCount()-1){
+                        //last round is partially visible: jump to end of this
+                        //round
+                        Rectangle r = getComponent(roundPanel).getBounds();
+                        return (r == null) ? 0 : (r.y+r.height - visibleRect.y);
+                    } else {
+                        //otherwise jump to start of next round
+                        Rectangle r = getComponent(roundPanel+1).getBounds();
+                        return (r == null) ? 0 : (r.y - visibleRect.y);
+                    }
+                } else { // i.e. north
+                    //rectangle of round closest to the top of the viewport
+                    Rectangle r = getComponent(roundPanel).getBounds();
+
+                    if ((r.y == visibleRect.y) && (roundPanel == 0))  {
+                        //the first round is completely visible, so we can't
+                        //scroll north
+                        return 0;
+                    } else if (r.y == visibleRect.y) {
+                        //the top round in the viewport is completely
+                        //visible, so jump to the round before that one
+                        Rectangle prevR = getComponent(roundPanel-1).getBounds();
+
+                        if (prevR == null || prevR.y >= r.y) {
+                            return 0;
+                        } else {
+                            return visibleRect.y - prevR.y;
+                        }
+                    } else {
+                        //the top round is partially visible: scroll to make
+                        //it completely visible
+                        return visibleRect.y - r.y;
+                    }
+                }
+            }
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public int getScrollableBlockIncrement(Rectangle visibleRect, int orientation, int direction) {
+        if(visibleRect!=null
+                && (orientation==SwingConstants.HORIZONTAL
+                    || orientation==SwingConstants.VERTICAL)){
+            //the round near the top of the viewport
+            int topRoundPanel = locationToRoundPanel(visibleRect.y);
+
+            //the round near the bottom of the viewport
+            int bottomRoundPanel = locationToRoundPanel(visibleRect.y + visibleRect.height);
+
+            if(topRoundPanel==-1 || bottomRoundPanel==-1){
+                return 0;
+            } else {
+                if (direction > 0) { // i.e. south
+                    //bottom round panel is placed at the top of the viewport
+                    //except when it is already completely visible, then the
+                    //next round is taken
+                    Rectangle r = getComponent(bottomRoundPanel).getBounds();
+                    if(r==null){
+                        return 0;
+                    } else if(bottomRoundPanel == getComponentCount()-1){
+                        //just jump to end of this round
+                        return (r.y + r.height - visibleRect.height) - visibleRect.y;
+                    } else if(visibleRect.y + visibleRect.height >= r.y + r.height
+                                || visibleRect.height < r.height) {
+                        //jump to the next round
+                        Rectangle nextR = getComponent(bottomRoundPanel+1).getBounds();
+                        if(nextR==null){
+                            return 0;
+                        } else {
+                            return nextR.y - visibleRect.y;
+                        }
+                    } else {
+                        //this is now guaranteed to be positive
+                        return r.y - visibleRect.y;
+                    }
+                } else { // i.e. north
+                    //TODO
+                    //if the top round panel is completely visible: it is placed
+                    //immediately after the viewport, otherwise it becomes the
+                    //bottom round panel
+
+                    //rectangle of round closest to the top of the viewport
+                    Rectangle r = getComponent(topRoundPanel).getBounds();
+
+                    if ((r.y == visibleRect.y) && (topRoundPanel == 0))  {
+                        //the first round is completely visible, so we can't
+                        //scroll north
+                        return 0;
+                    } else if (r.y == visibleRect.y) {
+                        //the top round in the viewport is completely
+                        //visible, so we place it immediately after the viewport
+
+                        if(visibleRect.y < visibleRect.height)
+                            return visibleRect.y;
+                        else
+                            return visibleRect.height;
+
+                    } else {
+                        //the top round is partially visible: scroll to make
+                        //it the bottom round
+                        if(visibleRect.height < r.height){
+                            return visibleRect.y - r.y;
+                        } else {
+                            //this is now guaranteed to be positive
+                            return (visibleRect.y +visibleRect.height - r.height) - r.y;
+                        }
+                    }
+                }
+            }
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private int locationToRoundPanel(int y){
+        if(getComponentCount()==0)
+            return -1;
+        else {
+            int roundPanel = 0;
+            int componentCount = getComponentCount();
+            while(roundPanel<componentCount && getComponent(roundPanel).getY() <= y){
+                roundPanel++;
+            }
+            return roundPanel-1;
+        }
+    }
+
+    public boolean getScrollableTracksViewportWidth() {
+        return true;
+    }
+
+    public boolean getScrollableTracksViewportHeight() {
+        return false;
     }
 }
