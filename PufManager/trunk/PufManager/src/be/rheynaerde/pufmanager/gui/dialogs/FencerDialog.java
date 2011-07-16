9065833e92c29132a83668dd4e901014fa4dd019
nico.vancleemput <nico.vancleemput@00d01c20-5a78-ff6a-2b06-9238665b289a>
2011-07-16 07:33:43 +0000

    Fixed imports

--- PufManager/trunk/PufManager/src/be/rheynaerde/pufmanager/gui/dialogs/FencerDialog.java
+++ PufManager/trunk/PufManager/src/be/rheynaerde/pufmanager/gui/dialogs/FencerDialog.java
@@ -32,8 +32,6 @@ import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ResourceBundle;
-import javax.swing.AbstractAction;
-import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
